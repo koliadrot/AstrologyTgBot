d17d23df38ce368146bc5594dc072016f0a3569b
Nikolai <koliadrot@mail.ru>
2023-10-13 08:06:18 +0200

    .net6.0+MariaDB

---
+++ TgBot/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MyEntity> MyEntities { get; set; }
+}
